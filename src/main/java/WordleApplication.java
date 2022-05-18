9260b4831d5590f5972a3028e31c39eed4105479
csabaszekelyhidi <szekelyhidicsaba92@gmail.com>
2022-05-18 16:05:53 +0200

    Character buttons are working - kind of

---
+++ src/main/java/WordleApplication.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class WordleApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Wordle.fxml"));
+        stage.setTitle("Wordle!");
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.show();
+    }
+
+}
