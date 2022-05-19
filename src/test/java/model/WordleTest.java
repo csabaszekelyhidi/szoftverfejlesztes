21ea1a98f9be595616fcc89665445b6b16edc538
csabaszekelyhidi <szekelyhidicsaba92@gmail.com>
2022-05-19 16:45:06 +0200

    Changing window size

--- src/test/java/model/WordleTest.java
+++ src/test/java/model/WordleTest.java
@@ -1,7 +1,6 @@
 package model;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
