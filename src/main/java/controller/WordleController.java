3c42348894590126c0fe35ea8dc6f3a6bb73d046
csabaszekelyhidi <szekelyhidicsaba92@gmail.com>
2022-05-19 08:16:24 +0200

    UI is completed

--- src/main/java/controller/WordleController.java
+++ src/main/java/controller/WordleController.java
@@ -2,50 +2,24 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
+import javafx.scene.paint.Color;
 
 public class WordleController {
 
     private int actual = 0;
+    private int currentStart = 0;
+
+    private boolean locked = false;
 
     @FXML
     private GridPane grid;
-
-    @FXML
-    private Text word11;
-    private Text word12;
-    private Text word13;
-    private Text word14;
-    private Text word15;
-    private Text word21;
-    private Text word22;
-    private Text word23;
-    private Text word24;
-    private Text word25;
-    private Text word31;
-    private Text word32;
-    private Text word33;
-    private Text word34;
-    private Text word35;
-    private Text word41;
-    private Text word42;
-    private Text word43;
-    private Text word44;
-    private Text word45;
-    private Text word51;
-    private Text word52;
-    private Text word53;
-    private Text word54;
-    private Text word55;
-    private Text word61;
-    private Text word62;
-    private Text word63;
-    private Text word64;
-    private Text word65;
 
     @FXML
     public void initialize() {
@@ -55,52 +29,41 @@ public class WordleController {
     public void calculate(ActionEvent actionEvent) {
         Button button = (Button) actionEvent.getSource();
 
-        if (button.getId().equals("but_Q")) {
-            Text txt = (Text) grid.getChildren().get(actual);
-            txt.setText("Q");
-        } else if (button.getId().equals("but_W")) {
-            Text txt = (Text) grid.getChildren().get(actual);
-            txt.setText("W");
-        } else if (button.getId().equals("but_E")) {
-            Text txt = (Text) grid.getChildren().get(actual);
-            txt.setText("E");
-        } else if (button.getId().equals("but_R")) {
-            Text txt = (Text) grid.getChildren().get(actual);
-            txt.setText("R");
-        } else if (button.getId().equals("but_T")) {
-            Text txt = (Text) grid.getChildren().get(actual);
-            txt.setText("T");
-        } else if (button.getId().equals("but_Y")) {
-            Text txt = (Text) grid.getChildren().get(actual);
-            txt.setText("Y");
-        } else if (button.getId().equals("but_U")) {
-            Text txt = (Text) grid.getChildren().get(actual);
-            txt.setText("U");
-        } else if (button.getId().equals("but_I")) {
-            Text txt = (Text) grid.getChildren().get(actual);
-            txt.setText("I");
-        } else if (button.getId().equals("but_O")) {
-            Text txt = (Text) grid.getChildren().get(actual);
-            txt.setText("O");
-        } else if (button.getId().equals("but_P")) {
-            Text txt = (Text) grid.getChildren().get(actual);
-            txt.setText("P");
-        }
 
+        if ( actual < currentStart +5 ) {
+            Label txt = (Label) grid.getChildren().get(actual);
+            txt.setText(button.getText());
 
             actual++;
+        }
     }
 
     public void backspace(ActionEvent actionEvent) {
-        Button button = (Button) actionEvent.getSource();
-        Text txt = (Text) grid.getChildren().get(actual-1);
-        txt.setText(" ");
-        if (actual > 0) {
+
+        if (actual > currentStart && actual <= currentStart +5) {
+            Label txt = (Label) grid.getChildren().get(actual-1);
+            txt.setText("");
             actual--;
         }
-
     }
 
+    public void enterWord(ActionEvent actionEvent) {
 
+        Label[] characters = new Label[5];
+        String wordguess = "";
+
+        if (actual%5 == 0) {
+
+            for (int i=0;i<5;i++) {
+                characters[i] = (Label) grid.getChildren().get(currentStart +i);
+                characters[i].setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+                wordguess = wordguess+characters[i].getText();
+            }
+
+            System.out.println(wordguess);
+
+            currentStart = currentStart + 5;
+        }
+    }
 
 }
